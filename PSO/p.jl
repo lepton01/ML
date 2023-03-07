b304c30bd44c78a6ea4f34ab519f8d216ff85f08
lepton01 <102274601+lepton01@users.noreply.github.com>
2023-03-06 22:41:47 -0600

    Add files via upload

---
+++ PSO/p.jl
@@ -0,0 +1,18 @@
+#06/03/2023
+
+using Statistics, Random, LinearAlgebra
+
+function a()
+    a::nothing
+end
+
+function main(pop::Int)
+    c1::Float64 = 0.49445
+    c2::Float64 = 0.49445
+    maxgen::Int = 100
+    Vmax = 0.5
+    Vmin = -0.5
+    popmax::Int = 2
+    popmin::Int = -2
+    P = Array{Float64}(undef, (pop))
+end
