a156e644f47a88c4d808cff4f6ab953c52a03355
lepton01 <102274601+lepton01@users.noreply.github.com>
2023-03-09 18:16:36 -0600

    Add files via upload

--- PSO/p.jl
+++ PSO/p.jl
@@ -3,65 +3,66 @@
 using Statistics, Random, LinearAlgebra
 Random.seed!(1)
 
-f(x, y) = x*sin(4x) + 1.1y*sin(2y)
-
-function fitness(P::AbstractArray)
+f(x::Real, y::Real) = x*sin(4x) + 1.1y*sin(2y)
+#=
+function fitness(P::AbstractArray, L::AbstractArray, g_best, g_best_fit)
     F = Vector{Float64}(undef, length(P[:, 1]))
     F[1] = f(P[1, 1], P[1, 2])
-    p_b = 1
 
     for i in 2:length(P[:, 1])
         F[i] = f(P[i, 1], P[i, 2])
-        if i >= 2 && F[i] < F[i - 1]
-            p_b = i
+        if F[i] < F[pop_best]
+            pop_best = i
         end
     end
-    F, p_b
+    F, pop_best
 end
 
-function mov(P::AbstractArray, fit::Vector, best::Int)
-    V = Array{Float64}(undef, size(P))
+function mov(P::AbstractArray, V::AbstractArray, fit::Vector, p_best::Int)
+    nothing
+end
+=#
 
+function PSOmax(pop_size::Int, para::Int = 2)
+    nothing
 end
 
-function PSOmin(pop::Int, para::Int = 2)
+function PSOmin(pop_size::Int, para::Int = 2)
     c1::Float64 = 0.49445
     c2::Float64 = 0.49445
 
     max_i::Int = 100
 
-    vmax = 0.5
-    vmin = -0.5
+    v_max = 0.5
+    v_min = -0.5
 
-    popmax::Int = 2
-    popmin::Int = -2
+    pop_max::Int = 20
+    pop_min::Int = -20
 
-    P = randn(Float64, (pop, para))
-    V = zeros(Float64, (pop, para))
-
-    
-end
-
-function PSOmax(pop::Int, para::Int = 2)
-    c1::Float64 = 0.49445
-    c2::Float64 = 0.49445
-
-    max_i::Int = 100
-
-    vmax = 0.5
-    vmin = -0.5
-
-    popmax::Int = 2
-    popmin::Int = -2
-
-    P = randn(Float64, (pop, para))
-    V = randn(Float64, (pop, para))
+    P = (pop_max - pop_min)*rand(Float64, (pop_size, para)) .+ pop_min
+    V = (v_max - v_min)*rand(Float64, (pop_size, para)) .+ v_min
+    L = P
+    g_best = (pop_max - pop_min)*rand(Float64, para) .+ pop_min
+    for i in 1:pop_size
+        if f(P[i, 1], P[i, 2]) < f(g_best[1], g_best[2])
+            g_best = P[i, :]
+        end
+    end
 
     for i in 1:max_i
-        fit, p_best = fitness(P)
-        S1 = Float64[]
-        S2 = Float64[]
-        
+        #fit, g_best = fitness(P, L, g_best, g_best_fit)
+        for j in 1:pop_size
+            V[j, 1] = V[j, 1] + c1*rand()*(L[j, 1] - P[j, 1]) + c2*rand()*(g_best[1] - P[j, 1])
+            V[j, 2] = V[j, 2] + c1*rand()*(L[j, 2] - P[j, 2]) + c2*rand()*(g_best[2] - P[j, 2])
+            P[j, 1] = P[j, 1] + V[j, 1]
+            P[j, 2] = P[j, 2] + V[j, 2]
+            if f(P[j, 1], P[j, 2]) < f(L[j, 1], L[j, 2])
+                L[j, 1], L[j, 2] = P[j, 1], P[j, 2]
+                if f(P[j, 1], P[j, 2]) < f(g_best[1], g_best[2])
+                    g_best = P[j, :]
+                end
+            end
+        end
     end
     P
 end
