5570d4006151e52b2ff249d7ca53ab673cddc598
lepton01 <102274601+lepton01@users.noreply.github.com>
2023-03-10 18:31:58 -0600

    Add files via upload

--- PSO/a01.jl
+++ PSO/a01.jl
@@ -1,5 +1,6 @@
 #06/03/2023
 
+using Plots
 using Statistics, Random, LinearAlgebra
 Random.seed!(1)
 
@@ -7,15 +8,15 @@ Random.seed!(1)
 
 
 #f(x::Real, y::Real) = sqrt(x*y)*sin(x)*sin(y)
-#v_max, v_min = 0, 10
+#pos_max::Int, pos_min::Int = 0, 10
 
-#f(x::Real, y::Real) = x^2 + 2y^2 - 0.3cos(3π*x) - 0.4cos(4π*y) + 0.7
+f(x::Real, y::Real) = x^2 + 2y^2 - 0.3cos(3π*x) - 0.4cos(4π*y) + 0.7
 #f(x::Real, y::Real) = x^2 + 2y^2 - 0.3cos(3π*x)*0.4cos(4π*y) + 0.3
-f(x::Real, y::Real) = x^2 + 2y^2 - 0.3cos(3π*x + 4π*y) + 0.3
-v_max::Int, v_min::Int = -100, 100
+#f(x::Real, y::Real) = x^2 + 2y^2 - 0.3cos(3π*x + 4π*y) + 0.3
+pos_max::Int, pos_min::Int = -100, 100
 
 #f(x::Real, y::Real) = (x + 2y -7)^2 + (2x + y -5)^2
-#v_max, v_min = -10, 10
+#pos_max::Int, pos_min::Int = -10, 10
 
 #=
 function fitness(P::AbstractArray, L::AbstractArray, g_best, g_best_fit)
@@ -85,16 +86,16 @@ function PSOmin(pop_size::Int = 10, max_i::Int = 100, para::Int = 2)
 
     #max_i::Int = 5000
 
-    #v_max = 0.5
-    #v_min = -0.5
+    v_max = 0.5
+    v_min = -0.5
 
-    pop_max::Int = 100
-    pop_min::Int = -100
+    #pop_max::Int = 100
+    #pop_min::Int = -100
 
-    P = (pop_max - pop_min)*rand(Float64, (pop_size, para)) .+ pop_min
+    P = (pos_max - pos_min)*rand(Float64, (pop_size, para)) .+ pos_min
     V = (v_max - v_min)*rand(Float64, (pop_size, para)) .+ v_min
     L = P
-    g_best = (pop_max - pop_min)*rand(Float64, para) .+ pop_min
+    g_best = (pos_max - pos_min)*rand(Float64, para) .+ pos_min
     for i in 1:pop_size
         if f(P[i, 1], P[i, 2]) < f(g_best[1], g_best[2])
             g_best = P[i, :]
@@ -108,13 +109,17 @@ function PSOmin(pop_size::Int = 10, max_i::Int = 100, para::Int = 2)
             V[j, 2] = V[j, 2] + c1*rand()*(L[j, 2] - P[j, 2]) + c2*rand()*(g_best[2] - P[j, 2])
             P[j, 1] = P[j, 1] + V[j, 1]
             P[j, 2] = P[j, 2] + V[j, 2]
-            if f(P[j, 1], P[j, 2]) < f(L[j, 1], L[j, 2])
-                L[j, 1], L[j, 2] = P[j, 1], P[j, 2]
-                if f(P[j, 1], P[j, 2]) < f(g_best[1], g_best[2])
-                    g_best = P[j, :]
+            if P[j, 1] < pos_min || P[j, 1] > pos_max || P[j, 2] < pos_min || P[j, 2] > pos_max
+                P[j, 1], P[j, 2] = P[j, 1] - V[j, 1], P[j, 2] - V[j, 2]
+            else
+                if f(P[j, 1], P[j, 2]) < f(L[j, 1], L[j, 2])
+                    L[j, 1], L[j, 2] = P[j, 1], P[j, 2]
+                    if f(P[j, 1], P[j, 2]) < f(g_best[1], g_best[2])
+                        g_best = P[j, :]
+                    end
                 end
             end
         end
     end
-    g_best, f(g_best[1], g_best[2])
+    println("Coords: $(g_best), Valor: $(f(g_best[1], g_best[2])), Prom_x:$(mean(L[:, 1])), Prom_y: $(mean(L[:, 2]))")
 end
