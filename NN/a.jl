2e5fd2cfd8fe4d6d9e503664d339a3d0b7662eed
lepton01 <102274601+lepton01@users.noreply.github.com>
2023-03-15 00:48:34 -0600

    Add files via upload

--- NN/a.jl
+++ NN/a.jl
@@ -1,13 +1,16 @@
 #11/03/2023
 
-using Plots
-using Statistics, Random, LinearAlgebra
-using MLJBase
 """
     Packages and dependencies.
 """
+using Plots
+using Statistics, Random, LinearAlgebra
+using MLJBase
 
 
+"""
+    Activation functions.
+"""
 sigmoid(x::Float64)::Float64 = 1/(1 + exp(-x))
 
 function sigmoid_back(x::Float64)::Float64
@@ -18,25 +21,26 @@ end
 ReLU(x::Float64)::Float64 = x > 0 ? x : 0
 
 ReLU_back(x::Float64)::Float64 = x > 0 ? 1 : 0
-"""
-    Activation functions.
-"""
 
 
+"""
+    Initialization of weights (wi) and biases (bi). Each layer is represented by a dictionary with keys being the names for the w and b of each neuron i.
+"""
 function init_para(layer_dims::Vector)::Dict{String, Array{Float64}}
     A = Dict{String, Array{Float64}}()
-    for i in 2:eachindex(layer_dims)
+    @inbounds for i in 2:eachindex(layer_dims)
         A[string("w", i - 1)] = rand(Float64, (layer_dims[i], layer_dims[i - 1]))/sqrt(i - 1)
         A[string("b", i - 1)] = zeros(Float64, layer_dims[i])
     end
+
     A
 end
-"""
-    Initialization of weights and biases.
-"""
 
 
-function fwd_prop(X::Array{Float64},
+"""
+    Forward propagation.
+"""
+@inline function fwd_prop(X::Array{Float64},
     para::Dict{String, Array{Float64}},
     aktiv::Tuple)
 
@@ -44,7 +48,7 @@ function fwd_prop(X::Array{Float64},
     cache = Dict{String, Array{Float64}}()
     cache[string("A", 0)] = X
 
-    for i in 1:n_layers
+    @inbounds for i in 1:n_layers
         begin
             wi = para[string("w", i)]
             Ai_ant = cache[string("A", i - 1)]
@@ -52,18 +56,18 @@ function fwd_prop(X::Array{Float64},
             mul!(yi, wi, Ai_ant)
             yi .+= para[string("b", i)]
         end
-        Ai = aktiv[i].(yi)
 
+        Ai = aktiv[i].(yi)
         cache[string("y", i)] = yi
         cache[string("A", i)] = Ai
     end
+
     Ai, cache
 end
-"""
-    Forward propagation.
-"""
 
-
+"""
+    Cost computation.
+"""
 function cost_bin(A::Array{Float64},
     B::Array{Float64})::Float64
 
@@ -71,12 +75,12 @@ function cost_bin(A::Array{Float64},
     cost = - sum(A.*log.(B) .+ (1 .- A).*log.(1 .- B))/length(A)
     cost
 end
-"""
-    Cost computation.
-"""
 
 
-function back_prop(A::Array,
+"""
+    Calculate changes in the parameters.
+"""
+@inline function back_prop(A::Array,
     B::Array,
     para::Array,
     cache::Array,
@@ -85,68 +89,88 @@ function back_prop(A::Array,
 
     n_layers = length(layer_dims)
     @assert length(A) == length(B) "Not the same size."
-    m = size(S)[2]
+    m = size(A)[2]
 
     dA = (-A/B .+ (1 .- A))./(1 .- B)
     if all(isnan.(dA))
         println("¡dA es NaN!")
         dA = rand(Float64)
     end
+
     grads = Dict{String, Array{Float64}}()
-    for i in n_layers - 1:-1:1
+    @inbounds for i in n_layers - 1:-1:1
         dy = dA.*aktiv[i].(cache[string("y", i)])
         grads[string("dw", i)] = 1/m.*(dy*transpose(cache[string("A", l-1)]))
         grads[string("db", i)] = 1/m.*sum(dy, dims = 2)
         dA = transpose(para[string("W", l)])*dy
     end
+
     grads
 end
-"""
-    Calculate changes in the parameters.
-"""
 
 
+"""
+    Update parameters.
+"""
 function up_para(para::Dict{String, Array{Float64}},
     grads::Dict{String, Array{Float64}},
     layer_dims::Array{Int},
     learn_rate::Number)::Dict{String, Array{Float64}}
 
     n_layers = length(layer_dims)
-    for i = 1:n_layers - 1
+    @inbounds for i = 1:n_layers - 1
         para[string("w", i)] -= learn_rate.*grads[string("dw", i)]
         para[string("b", i)] -= learn_rate.*grads[string("db", i)]
     end
+
     para
 end
-"""
-    Update parameters.
-"""
 
 
-function get_activ(aktiv)
-    aktiv_back = []
-    for activation in aktiv
-        push!(aktiv_back, @eval($(Symbol("$activation", "_back"))))
-    end
-    aktiv_back = Tuple(aktiv_back)
-    aktiv_back
-end
 """
     Obtain the activations.
 """
+function get_activ(aktiv)
+    aktiv_back = []
+    @inbounds for activation in aktiv
+        push!(aktiv_back, @eval($(Symbol("$activation", "_back"))))
+    end
+
+    aktiv_back = Tuple(aktiv_back)
+    aktiv_back
+end
 
 
+"""
+    ...
+"""
 function reshape_A(A::Array)
     A = convert(Array{Float64, ndims(A)}, A)
     A = reshape(A, 1, :)
     A
 end
-"""
-    ...
-"""
 
 
-function neural_net_dense(X, Y,
+"""
+    neural_network_dense(X, Y, layer_dims::Array{Int}, num_iterations::Int, learning_rate::Number; activations=Nothing, print_stats=false, parameters=nothing, resume=false, checkpoint_steps=100)
+    Build and train a dense neural network for binary classification. Performs batch gradient descent for optimization and uses binary logistic loss for cost function.
+    Also supports resuming training from previously trained parameters.
+    Parameters:
+    - `X`: training inputs (the first value in size of this should be the same as the first value in layer_dims)
+    - `Y`: training outputs (for binary classification, first value of size should be 1)
+    - `layer_dims`: Number of neurons in each layer. First layer size should be equal to the number of features in input. Output layer should be 1 in case of binary classification
+    - `num_iterations`: Number of iterations to train for.
+    - `learning_rate`: for gradient descent optimizer
+    - `activations` (optional): A tuple of activation functions for every layer other than the input layer (default: (relu, relu...., sigmoid))
+    - `print_stats` (optional): Whether to print mean and variance of parameters for every `checkpoint_steps` steps (Statistics package must be imported to use this) (default: false)
+    - `checkpoint_steps` (optional): the loss (and stats, if print_stats is true) is printed every `checkpoint_steps` steps (default: 100)
+    - `resume` (optional): whether to resume training by using the given parameters (if parameters are not given, they are initialized again) (default: false)
+    - `parameters` (optional): parameters for resuming training from checkpoint. Used only if resume is true (default: nothing)
+    Returns:
+    - `parameters`: trained parameters
+    - `activations`: activations used in training (for passing them to `predict` function)
+"""
+@inline function neural_net_dense(X, Y,
     layer_dims::Array{Int},
     n_it::Int,
     learn_rate::Number;
@@ -163,11 +187,13 @@ function neural_net_dense(X, Y,
     begin
         if activations === nothing
             activations = Array{Function}(undef, num_layers-1)
-            for i = 1:n_layers - 2
+            @inbounds for i = 1:n_layers - 2
                 activations[i] = relu
             end
+
             activations[n_layers - 1] = sigmoid
         end
+
         activations = Tuple(activations)
         activations_back = get_back_activations(activations)
     end
@@ -213,29 +239,23 @@ function neural_net_dense(X, Y,
             end
         end
     end
+
     parameters, activations
 end
+
+
 """
-    neural_network_dense(X, Y, layer_dims::Array{Int}, num_iterations::Int, learning_rate::Number; activations=Nothing, print_stats=false, parameters=nothing, resume=false, checkpoint_steps=100)
-    Build and train a dense neural network for binary classification. Performs batch gradient descent for optimization and uses binary logistic loss for cost function.
-    Also supports resuming training from previously trained parameters.
+    predict(X, Y, parameters, activations::Tuple)
+    Predict using the trained parameters and calculate the accuracy.
     Parameters:
-    - `X`: training inputs (the first value in size of this should be the same as the first value in layer_dims)
-    - `Y`: training outputs (for binary classification, first value of size should be 1)
-    - `layer_dims`: Number of neurons in each layer. First layer size should be equal to the number of features in input. Output layer should be 1 in case of binary classification
-    - `num_iterations`: Number of iterations to train for.
-    - `learning_rate`: for gradient descent optimizer
-    - `activations` (optional): A tuple of activation functions for every layer other than the input layer (default: (relu, relu...., sigmoid))
-    - `print_stats` (optional): Whether to print mean and variance of parameters for every `checkpoint_steps` steps (Statistics package must be imported to use this) (default: false)
-    - `checkpoint_steps` (optional): the loss (and stats, if print_stats is true) is printed every `checkpoint_steps` steps (default: 100)
-    - `resume` (optional): whether to resume training by using the given parameters (if parameters are not given, they are initialized again) (default: false)
-    - `parameters` (optional): parameters for resuming training from checkpoint. Used only if resume is true (default: nothing)
+    - `X`: testing inputs
+    - `Y`: outputs to check with
+    - `parameters`: trained parameters which is taken from the output of `neural_network_dense`
+    - `activations`: also given by the outputs of `neural_network_dense`
     Returns:
-    - `parameters`: trained parameters
-    - `activations`: activations used in training (for passing them to `predict` function)
+    - `predicts`: predictions from the NN
+    - `accuracy`: accuracy of predictions
 """
-
-
 function predict(X, Y, para::Array, activations::Tuple)
     m = size(X)[2]
     n = length(para)
@@ -262,15 +282,3 @@ function predict(X, Y, para::Array, activations::Tuple)
     end
     predicts, accuracy
 end
-"""
-    predict(X, Y, parameters, activations::Tuple)
-    Predict using the trained parameters and calculate the accuracy.
-    Parameters:
-    - `X`: testing inputs
-    - `Y`: outputs to check with
-    - `parameters`: trained parameters which is taken from the output of `neural_network_dense`
-    - `activations`: also given by the outputs of `neural_network_dense`
-    Returns:
-    - `predicts`: predictions from the NN
-    - `accuracy`: accuracy of predictions
-"""
