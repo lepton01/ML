a3ab5aad6c64937d2e8bd18b70d61eac37541cf9
Angel Ortiz <ang3lm4rioortiz@gmail.com>
2023-04-04 17:41:22 -0600

    martes 042341Z - major changes to using Flux

--- NN/bessel_1.jl
+++ NN/bessel_1.jl
@@ -3,7 +3,7 @@
 using LinearAlgebra, Statistics, Random
 using Plots
 using Flux, SpecialFunctions
-using Flux: crossentropy, train!, params
+using Flux: mse, train!
 
 Random.seed!(1)
 """
@@ -11,37 +11,38 @@ Random.seed!(1)
 
 ¿? SOMETHING
 """
-function main(x::Vector{Float32}, z::Float32, ep::Int = 1000)
-    y = map(x) do a
-        real(besselj(z, a))
+function main(x::Vector{Float32}, z::Float32, ep::Int = 1_000)
+    target = map(x) do i
+        real(besselj(z, i))
     end
-    p = plot(x, y)
+    
+    p = plot(x, target)
     
     X = vcat(x', zeros(Float32, length(x))')
     X[2, :] .= z
+    loader = Flux.DataLoader((X, target), batchsize = 64, shuffle = true)
+
     model = Chain(
-        Dense(2, 8, relu),
-        Dense(8, 16, relu),
-        Dense(16, 8, relu),
-        Dense(8, 1),
+        Dense(2 => 2, relu),
+        Dense(2 => 4, relu),
+        Dense(4 => 2, relu),
+        Dense(2 => 1),
     softmax)
 
-    loss(x, y) = crossentropy(model(x), y)
-    para = params(model)
+    loss(m, x, y) = mse(m(x), y)
     lr::Float32 = 0.01
-    opt = ADAM(lr)
+    opt_st = Flux.setup(Adam(), model)
     loss_h = Float32[]
     for i ∈ 1:ep
-        train!(loss, para, [(X, y')], opt)
-        train_l = loss(X, y')
+        train!(loss, model, [(X, target')], opt_st)
+        train_l = loss(model, X, target')
         push!(loss_h, train_l)
         println("Epoch = $i. Training loss = $train_l")
     end
     ŷ = model(X)
     p = plot!(x, ŷ')
     savefig(p, "besselj")
-
-    return mean(ŷ .== y')*100
+    return mean(ŷ .== target')*100
 end
 
 main(collect(Float32, LinRange(0.01, 10, 1000)), Float32(1.5))
