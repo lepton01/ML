3ad571f53f20f19ef5983290751fed434b4ed5e8
Angel Ortiz <ang3lm4rioortiz@gmail.com>
2023-04-06 13:14:54 -0600

    jueves 061914Z - bessel appx changes - ¡MORE NEURONS! -relu working 72.84%

--- NN/bessel_1.jl
+++ NN/bessel_1.jl
@@ -18,61 +18,55 @@ function main(x::Vector{Float32}, a::Float32, ep::Int = 10_000)
     X_train = vcat(x', fill(a, (1, length(x))))
     train_SET = [(X_train, Y_train')]
     model = Chain(
-        Dense(2 => 16),
-        Dense(16 => 32),
-        Dense(32 => 64),
-        Dense(64 => 32),
-        Dense(32 => 16),
+        Dense(2 => 16, relu),
+        Dense(16 => 32, relu),
+        Dense(32 => 64, relu),
+        Dense(64 => 32, relu),
+        Dense(32 => 16, relu),
         Dense(16 => 1)
     )
+    #=
+    model = Chain(
+        Dense(2 => 16, relu),
+        Dense(16 => 32, relu),
+        Dense(32 => 64, relu),
+        Dense(64 => 32, relu),
+        Dense(32 => 16, relu),
+        Dense(16 => 1)
+    )
+    model = Chain(
+        Dense(2 => 16, relu),
+        Dense(16 => 32, relu),
+        Dense(32 => 64, relu),
+        Dense(64 => 32, relu),
+        Dense(32 => 16, relu),
+        Dense(16 => 1)
+    )
+    model = Chain(
+        Dense(2 => 16, relu),
+        Dense(16 => 32, relu),
+        Dense(32 => 64, relu),
+        Dense(64 => 32, relu),
+        Dense(32 => 16, relu),
+        Dense(16 => 1)
+    )
+    =#
     loss(m, x, y) = mae(m(x), y)
     opt = Flux.setup(Flux.Adam(), model)
     loss_log = Float32[]
-    for i ∈ 1:ep
-        
+    for i ∈ 1:ep        
         train!(loss, model, train_SET, opt)
         train_loss = loss(model, X_train, Y_train')
         push!(loss_log, train_loss)
         if rem(i, 100) == 0
             println("Epoch = $i. Training loss = $train_loss")
         end
-        
-        #=
-        losses = Float32[]
-        for (i, data) ∈ enumerate(train_SET)
-            input, label = data
-
-            val, grads = Flux.withgradient(model) do m
-                # Any code inside here is differentiated.
-                # Evaluation of the model and loss must be inside!
-                result = m(input)
-                mse(result, label)
-            end
-            # Save the loss from the forward pass. (Done outside of gradient.)
-            push!(losses, val)
-            # Detect loss of Inf or NaN. Print a warning, and then skip update!
-            #=
-            if !isfinite(val)
-                @warn "loss is $val on item $i." i
-                continue
-            end
-            =#
-            Flux.update!(opt, model, grads[1])
-        end
-        train_loss = loss(model, X_train, Y_train')
-        if i%100 == 0
-            println("Epoch = $i. Training loss = $train_loss")
-        end
-        # Compute some accuracy, and save details as a NamedTuple
-        acc = mean(model(X_train) .≈ Y_train')
-        #push!(loss_log, losses)
-
         # Stop training when some criterion is reached
+        acc = mean(isapprox.(model(X_train), Y_train'; atol = 0.05))
         if acc > 0.95
             println("stopping after $epoch epochs.")
             break
         end
-        =#
     end
     Y_hat = model(X_train)
     p = plot(x, Y_train)
